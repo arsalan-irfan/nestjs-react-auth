b476227314753ecb762db920700dc46187901c79
Muhammad Arsalan <43111371+arsalan318@users.noreply.github.com>
2024-02-25 12:42:21 +0500

    fix(auth): fixed the cors issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({ credentials: true, origin: 'http://localhost:3000' });
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
